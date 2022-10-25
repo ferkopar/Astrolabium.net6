3779b61339ce14340a95807c33ff3b462d136db7
Párdi Ferenc <ferkopar@gmail.com>
2022-10-25 23:46:30 +0200

    Fonts, helpers

---
+++ Astrolabium/Helpers/WheelGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Astrolabium.Models;
+using System.IO;
+using Astrolabium.Models;
+
+namespace Astrolabium.Helpers
+{
+    public static class WheelGenerator
+    {
+        public static Image GenerateWheel(EphemerisResult r,int x,int y)
+        {
+            var bitmap = new Bitmap(x, y);
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.DrawLine(Pens.White, 1, 1, x - 1, y - 1);
+            }
+            return bitmap;
+        }
+    }
+}
