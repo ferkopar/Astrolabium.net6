826bbe5f99566ea5ec7224fd46b65041b213741b
Párdi Ferenc <ferkopar@gmail.com>
2022-10-26 00:31:04 +0200

    text

---
+++ Astrolabium.Text/AstrolabiumText.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astrolabium.Text
+{
+    public class AstrolabiumText
+    {
+        public string Head { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Astrolabium.Text/[enum].cs
@@ -0,0 +1,10 @@
+﻿namespace Astrolabium.Text
+{
+    public enum ElementType
+    {
+        General = 0,
+        BodyInSign = 1,
+        BodyInHouse = 2,
+        BodyAspect =3
+    }
+}
